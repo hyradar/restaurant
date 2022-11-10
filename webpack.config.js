916de25d7c49471a74f059ac471af656257123c1
Hunter Yeago <hyradar@tutamail.com>
2022-11-10 14:53:28 +1100

    compressed background image, installed dev dependencies, html and css skeleton

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,25 @@
-
+const path = require('path');
 
 module.exports = {
-    mode: 'development'
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    devServer: {
+        static: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    }
+    // ,
+    // optimization: {
+    //     runtimeChunk: 'single',
+    // },
 };
