fb04aca228c1d7f0f4b5d4d11cc095232b4d2078
Hunter Yeago <hyradar@tutamail.com>
2022-11-15 10:15:53 +1100

    fixed dropdown not dissapearing on window click

--- src/script.js
+++ src/script.js
@@ -1,17 +1,31 @@
 // For explanation, watch https://www.youtube.com/watch?v=S-VeYcOCFZw
 
 document.addEventListener('click', e => {
-    const isDropDownButton = e.target.matches("[data=dropdown-button]");
-    if (!isDropDownButton && e.target.closest('[data-dropdown]') != null) return;
-
     let currentDropDown;
+    const isDropDownButton = e.target.matches("[data=dropdown-button]");
+
+    // Makes sure not to close dropdown if user clicks inside it
+    if (!isDropDownButton && e.target.closest('[data-dropdown]') != null) {
+        return;
+        
+        //Closes Dropdown if Window is Clicked
+        //added the check against mobile menu button since both this if and the Toggle if's were firing simultaneously
+    } else if (window && e.target.closest('[data-dropdown]') === null && e.target.closest('.dropdown-menu') === null && e.target.closest('.mobilemenubutton') === null) {
+        currentDropDown = document.querySelector('.dropdown');
+        currentDropDown.classList.remove('active');
+        console.log('window firing');
+    };
+    
+    //Toggles dropdown if button is clicked
     if (isDropDownButton) {
         currentDropDown = document.querySelector('.dropdown');
         currentDropDown.classList.toggle('active');
+        console.log('dropdownriring');
     }
 
-    document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
-        if (dropdown === currentDropDown) return
-        dropdown.classList.remove('active')
-    })
+    //This code doens't appear to do anything, but didn't want to delete it yet
+    // document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+    //     if (dropdown === currentDropDown) return
+    //     dropdown.classList.remove('active')
+    // })  
 });
